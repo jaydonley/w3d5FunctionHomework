4e204d07da8bbf3e93d73866a1bd36b2040c29d2
Jason Donley <jay.donley@gmail.com>
2022-10-16 08:46:04 -0400

    w3d5FunctionHomework

---
+++ app.js
@@ -0,0 +1,80 @@
+var a = 10
+var b = 5
+
+function totalSum(param1, param2){
+    return param1 + param2;
+}
+
+console.log(totalSum(a,b));
+
+
+
+var totalSum = (param1, param2) => {
+    return param1 + param2;
+}
+
+console.log(totalSum(a,b));
+
+
+//-----------------------------------------------------
+
+
+
+function totalDiff(param1, param2){
+    return param1 - param2;
+}
+
+console.log(totalDiff(a,b));
+
+
+
+var totalDiff = (param1, param2) => {
+    return param1 - param2;
+}
+
+console.log(totalDiff(a,b));
+
+
+
+
+
+//----------------------------------------------------
+
+function totalProd(param1, param2){
+    return param1 * param2;
+}
+
+console.log(totalProd(a,b));
+
+
+
+var totalProd = (param1, param2) => {
+    return param1 * param2;
+}
+
+console.log(totalProd(a,b));
+
+
+
+
+
+//-----------------------------------------------
+
+function totalQuot(param1, param2){
+    return param1 / param2;
+}
+
+console.log(totalQuot(a,b));
+
+
+
+var totalQuot = (param1, param2) => {
+    return param1 / param2;
+}
+
+console.log(totalQuot(a,b));
+
+
+
+
+
